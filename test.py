4358d1edc301a1d0113ab223166d8702a98dd14d
Youssef Abali <youssef.abali.713@gmail.com>
2020-08-24 12:26:23 +0100

    changed test pin and sensor

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import Adafruit_DHT
 import time
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 3
 
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
