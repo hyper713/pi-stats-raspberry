b709dd8967ca29b58d15913408436d73055fed75
Youssef Abali <youssef.abali.713@gmail.com>
2020-08-28 21:47:08 +0100

    added requests exceptions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import Adafruit_DHT
 import requests
 import time
+from requests import exceptions
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 3
@@ -12,11 +13,17 @@ while True:
     raspberry_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
     if humidity is not None and temperature is not None:
-        r = requests.post(host, json={"temp": str(round(temperature, 1)), "rh": str(
-            round(humidity, 1)), "time": str(raspberry_time), "key": str(app_key)})
-        print(r.json()["msg"])
-        time.sleep(60)
+        try:
+            r = requests.post(host, json={"temp": str(round(temperature, 1)), "rh": str(
+                round(humidity, 1)), "time": str(raspberry_time), "key": str(app_key)})
+            print(r.json()["msg"])
+            time.sleep(10)
+        except requests.exceptions.RequestException as e:
+            time.sleep(10)
     else:
-        r = requests.post(host, json={
-                          "temp": "-", "rh": "-", "time": str(raspberry_time), "key": str(app_key)})
-        print(r.json()["msg"])
+        try:
+            r = requests.post(host, json={
+                "temp": "-", "rh": "-", "time": str(raspberry_time), "key": str(app_key)})
+            print(r.json()["msg"])
+        except requests.exceptions.RequestException as e:
+            time.sleep(10)
